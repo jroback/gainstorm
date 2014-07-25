49a6daf744be9ac8123927715f3edbbcb68d35aa
jroback <jerryroback@Jerrys-MacBook-Pro.local>
2014-07-24 23:24:09 -0600

    updates

---
+++ vendor/bundle/ruby/2.0.0/gems/pg-0.17.1/ext/siteconf20140724-68341-1mlo0cx.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140724-68341-1vbst2j"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
