5c8c943eec5622b8c8a48f5594906bbf63c11973
jroback <jerryroback@Jerrys-MacBook-Pro.local>
2014-07-24 18:18:30 -0600

    semi working

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :posts
 
   get 'pages/story'
-
-  get 'pages/about'
+ root 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
